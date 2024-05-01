be2e2f1f524f42cda2ba43233ced02c24bed9f32
EasySwap <142835812+EziSwap@users.noreply.github.com>
2024-05-02 00:59:41 +0300

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-cbd44f26-4827-4c2e-b78a-d4dbd7e0a616/"
 
 OUTPUT_DIR="logs"
 
-eth_data=$(curl -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","id":1}' "$RPC_URL")
+UA=$(shuf -n 1 ua.txt)
+
+axelar_data=$(curl -A UA -x http://104.239.10.73:5744 --proxy-user tihhlvxl:qatvzutcsmo4 -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
         mkdir "$OUTPUT_DIR"
     fi
-    
-    echo "$eth_data" >> "$OUTPUT_DIR/ethereum.txt"
-    
-    echo "Ethereum data fetched and saved to $OUTPUT_DIR/ethereum.txt."
+
+    echo "$axelar_data" >> "$OUTPUT_DIR/axelar.txt"
+
+    echo "Axelar data fetched and saved to $OUTPUT_DIR/axelar.txt."
 else
-    echo "Failed to fetch Ethereum data."
+    echo "Failed to fetch Axelar data."
 fi
